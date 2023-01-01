d63563d7c300367022f72982cceaa2bd07cb8fd9
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2023-01-02 00:02:29 +0330

    first commit

---
+++ src/test/java/ir/sheikhoo/RabbitMQSpringBoot/RabbitMqSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.sheikhoo.RabbitMQSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
